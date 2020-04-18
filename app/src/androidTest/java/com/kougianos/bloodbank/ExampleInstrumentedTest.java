b143d6997a05056baafc9fb5629ff2d34435e6c1
Nikos Kougianos <kougi_jr@hotmail.com>
2020-04-19 01:38:10 +0300

    Change application name

--- app/src/androidTest/java/com/kougianos/bloodbank/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/kougianos/bloodbank/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.kougianos.blood", appContext.getPackageName());
+        assertEquals("com.kougianos.bloodbank", appContext.getPackageName());
     }
 }
