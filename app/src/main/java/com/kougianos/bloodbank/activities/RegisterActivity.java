e993860e3c2fd8fbccee413f355a8cb7d2845d59
Nikos Kougianos <kougi_jr@hotmail.com>
2020-04-20 23:21:08 +0300

    [RegisterActivity]Scroll to bottom on password focus

--- app/src/main/java/com/kougianos/bloodbank/activities/RegisterActivity.java
+++ app/src/main/java/com/kougianos/bloodbank/activities/RegisterActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.kougianos.bloodbank.R;
@@ -16,17 +17,33 @@ public class RegisterActivity extends AppCompatActivity {
 
     private EditText nameEt, cityEt, mobileEt, bloodgroupEt, passwordEt;
     private Button submitButton;
+    private ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        scrollView = findViewById(R.id.scroll_view);
         nameEt = findViewById(R.id.name);
         cityEt = findViewById(R.id.city);
         passwordEt = findViewById(R.id.password);
         bloodgroupEt = findViewById(R.id.blood_group);
         mobileEt = findViewById(R.id.mobile);
         submitButton = findViewById(R.id.submit_button);
+
+        passwordEt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    View lastChild = scrollView.getChildAt(scrollView.getChildCount() - 1);
+                    int bottom = lastChild.getBottom() + scrollView.getPaddingBottom();
+                    int sy = scrollView.getScrollY();
+                    int sh = scrollView.getHeight();
+                    int delta = bottom - (sy + sh);
+                    scrollView.smoothScrollBy(0, delta);
+                }
+            }
+        });
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
