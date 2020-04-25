b4400bf7d3441fa0200ecd295f4118491f882cc0
Nikos Kougianos <kougi_jr@hotmail.com>
2020-04-26 00:16:33 +0300

    register backend implementation

---
+++ app/src/main/java/com/kougianos/bloodbank/Utils/Endpoints.java
@@ -0,0 +1,7 @@
+package com.kougianos.bloodbank.Utils;
+
+public class Endpoints {
+
+    public static final String baseUrl = "https://nikolas-opalios.000webhostapp.com/";
+    public static final String registerUrl = baseUrl + "register.php";
+}
