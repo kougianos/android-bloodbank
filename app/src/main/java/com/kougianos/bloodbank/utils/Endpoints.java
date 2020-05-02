b087aeb6bdbc12552527f20cd3686b4bb41dc2a1
Nikos Kougianos <kougi_jr@hotmail.com>
2020-05-03 02:04:17 +0300

    Add private constructor on Endpoints

--- app/src/main/java/com/kougianos/bloodbank/utils/Endpoints.java
+++ app/src/main/java/com/kougianos/bloodbank/utils/Endpoints.java
@@ -2,6 +2,11 @@ package com.kougianos.bloodbank.utils;
 
 public class Endpoints {
 
+    // Private constructor
+    private Endpoints() {
+
+    }
+
     private static final String baseUrl = "https://nikolas-opalios.000webhostapp.com/";
     public static final String registerUrl = baseUrl + "register.php";
     public static final String loginUrl = baseUrl + "login.php";
