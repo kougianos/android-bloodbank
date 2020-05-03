85872c1ccb63aa3c48e1e04ed8d1c0e155d5303f
Nikos Kougianos <kougi_jr@hotmail.com>
2020-05-03 17:55:48 +0300

    Complete make request activity

--- app/src/main/java/com/kougianos/bloodbank/activities/MakeRequestActivity.java
+++ app/src/main/java/com/kougianos/bloodbank/activities/MakeRequestActivity.java
@@ -1,44 +1,71 @@
 package com.kougianos.bloodbank.activities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.PermissionChecker;
+import androidx.preference.PreferenceManager;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.androidnetworking.interfaces.UploadProgressListener;
 import com.bumptech.glide.Glide;
 import com.kougianos.bloodbank.R;
+import com.kougianos.bloodbank.utils.Endpoints;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.net.URISyntaxException;
 
 public class MakeRequestActivity extends AppCompatActivity {
 
-    EditText messageText;
-    TextView chooseImageText;
-    ImageView postImage;
-    Button submitButton;
-    Uri imageUri;
+    private EditText messageText;
+    private TextView chooseImageText;
+    private ImageView postImage;
+    private Button submitButton;
+    private Uri imageUri;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_request);
+        AndroidNetworking.initialize(getApplicationContext());
 
         messageText = findViewById(R.id.message);
         chooseImageText = findViewById(R.id.choose_image);
         postImage = findViewById(R.id.post_image);
         submitButton = findViewById(R.id.submit_button);
+        progressBar = findViewById(R.id.progressBar_cyclic);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 if (isValid()) {
+                    progressBar.setVisibility(View.VISIBLE);
                     uploadRequest(messageText.getText().toString());
                 }
 
@@ -49,20 +76,92 @@ public class MakeRequestActivity extends AppCompatActivity {
         chooseImageText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                startActivityForResult(intent, 101);
-
+                permission();
             }
         });
     }
 
-    private void uploadRequest(String message) {
-        //code to upload message
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, 101);
     }
 
-    private void uploadImage() {
-        //code to upload image
+    private void permission() {
+        if (PermissionChecker.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PermissionChecker.PERMISSION_GRANTED) {
+            //ask for permission
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 401);
+        } else {
+            pickImage();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 401) {
+            if (grantResults[0] == PermissionChecker.PERMISSION_GRANTED) {
+                //permission was granted
+            } else {
+                //permission not granted
+                showMessage("Permission Declined");
+            }
+        }
+    }
+
+    private void uploadRequest(String message) {
+        //code to upload message and photo
+        String path = "";
+        try {
+            path = getPath(imageUri);
+        } catch (URISyntaxException e) {
+            showMessage("Wrong URI");
+        }
+
+        // Get number from shared preferences
+        String number = PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+                .getString("number", "12345");
+
+        AndroidNetworking.upload(Endpoints.uploadUrl)
+                .addMultipartFile("file", new File(path))
+                .addQueryParameter("message", message)
+                .addQueryParameter("number", number)
+                .setPriority(Priority.HIGH)
+                .build()
+                .setUploadProgressListener(new UploadProgressListener() {
+                    @Override
+                    public void onProgress(long bytesUploaded, long totalBytes) {
+
+                        // Do nothing
+
+                    }
+                })
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        progressBar.setVisibility(View.INVISIBLE);
+
+                        try {
+                            if (response.getBoolean("success")) {
+                                showMessage("Your post was successfully uploaded!");
+                                MakeRequestActivity.this.finish();
+                            } else {
+                                showMessage(response.getString("message"));
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        // do nothing
+                    }
+
+                });
     }
 
     // Handle image with specific request code
@@ -82,6 +181,9 @@ public class MakeRequestActivity extends AppCompatActivity {
         if (messageText.getText().toString().isEmpty()) {
             showMessage("Your message is empty, please write something");
             return false;
+        } else if (imageUri == null) {
+            showMessage("Please select an image to upload");
+            return false;
         }
 
         return true;
@@ -89,6 +191,73 @@ public class MakeRequestActivity extends AppCompatActivity {
 
     private void showMessage(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    @SuppressLint("NewApi")
+    private String getPath(Uri uri) throws URISyntaxException {
+        String selection = null;
+        String[] selectionArgs = null;
+        // Uri is different in versions after KITKAT (Android 4.4), we need to
+        // deal with different Uris.
+        if (DocumentsContract.isDocumentUri(getApplicationContext(), uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                return Environment.getExternalStorageDirectory() + "/" + split[1];
+            } else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                uri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.parseLong(id));
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("image".equals(type)) {
+                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                selection = "_id=?";
+                selectionArgs = new String[]{
+                        split[1]
+                };
+            }
+        }
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = {
+                    MediaStore.Images.Media.DATA
+            };
+            Cursor cursor = null;
+            try {
+                cursor = getContentResolver()
+                        .query(uri, projection, selection, selectionArgs, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 
 }
